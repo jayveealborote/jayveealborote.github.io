886048004785addaa8f70a190fd3d1f1085b1fc3
Jayvee Alborote <jayveekun@live.com>
2019-01-15 16:25:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('login-app').then(function(cache) {
      return cache.addAll([
-       '/t2app/',
-       '/t2app/index.html',
-       '/t2app/style.css',
-       '/t2app/index.js'
+       '/',
+       '/index.html',
+       '/style.css',
+       '/index.js'
 
      ]);
    })
